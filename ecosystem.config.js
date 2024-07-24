59e4207b4a55859b48a0e84915c1ac7855997f07
Jonathan Parra <jonathan01.07.95@gmail.com>
2024-07-24 16:40:27 -0500

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,25 @@ module.exports = {
         watch: true, // Opcional: reinicia si hay cambios en el script
         autorestart: true,
         instances: 1, // Número de instancias (o 'max' para el máximo número de instancias posibles)
+        env: {
+            HOST:localhost,
+            PORT:3001,
+            MAX_USER_MESSAGE_LENGTH:200,
+            MAX_SUGGESTIONS_NUMBER:2,
+            SYNTAX_CLEANER:True,
+            STOP_WORDS_CLEANER:False,
+            LEMMATIZER:False,
+            CONFIDENCE_LIMIT:0.70,
+            SUGGESTION_LIMIT:0.60,            
+            BASE_MODEL:sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2,
+            FINED_TUNED_MODEL:fis-chatbot/tuned-api-core,
+            MODEL_TOKEN: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,
+            UTTERANCES_LIMIT:5,
+            BATCH_SIZE:16,
+            EPOCHS:12,
+            DB_URL: 'http://localhost:3000/api'
+            
+          }
       },
     ],
   };
